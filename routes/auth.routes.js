eb56b771e4f94298be7d04110ec3895fa0cf8998
huperv <gulko.vlad@gmail.com>
2022-04-09 23:49:19 +0300

    1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -20,7 +20,7 @@ router.post(
       if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
-          message: 'Некорректные данные при регистрации',
+          message: ' Некорректные данные при регистрации',
         })
       }
       const { email, password } = req.body
